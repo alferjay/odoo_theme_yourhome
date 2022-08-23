3a1c5191f3ec2778a92ab1f239f140205b11d2c2
= <=>
2022-08-23 06:56:32 +0400

    initial commit

---
+++ theme_yourhome/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'YourHome Theme',
+    'description': 'YourHome website theme',
+    'category': 'Theme',
+    'sequence': 10,
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [
+        'views/header.xml',
+        'views/footer.xml',
+        'views/homepage.xml',
+    ],
+    'assets':{
+        'web.assets_frontend': [
+            'theme_yourhome/static/src/scss/styles.scss'
+        ],
+        'web._assets_primary_variables': [
+            "theme_yourhome/static/src/scss/primary_variables.scss",
+        ]
+    },
+    'images': [
+    ],
+    'snippet_lists': {
+    },
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
